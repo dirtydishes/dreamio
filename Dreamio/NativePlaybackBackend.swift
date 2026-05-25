7e9bc35e61ae549c13a4393ea0ea8dcd9ff1432e
dirtydishes <dishes@dpdrm.com>
2026-05-25 11:01:51 -0400

    add native audio track selection

--- Dreamio/NativePlaybackBackend.swift
+++ Dreamio/NativePlaybackBackend.swift
@@ -6,12 +6,15 @@ protocol NativePlaybackBackend: AnyObject {
     var onFailure: ((Error) -> Void)? { get set }
     var onStateChange: (() -> Void)? { get set }
     var onSubtitleTracksChange: (() -> Void)? { get set }
+    var onAudioTracksChange: (() -> Void)? { get set }
     var isPlaying: Bool { get }
     var isSeekable: Bool { get }
     var duration: TimeInterval { get }
     var currentTime: TimeInterval { get }
     var remainingTime: TimeInterval { get }
     var position: Float { get }
+    var audioTracks: [AudioTrack] { get }
+    var selectedAudioTrackID: Int32 { get }
     var subtitleTracks: [SubtitleTrack] { get }
     var selectedSubtitleTrackID: Int32 { get }
     var subtitleDelay: TimeInterval { get }
@@ -23,6 +26,7 @@ protocol NativePlaybackBackend: AnyObject {
     func togglePlayPause()
     func seek(to position: Float)
     func jump(by seconds: TimeInterval)
+    func selectAudioTrack(id: Int32)
     func selectSubtitleTrack(id: Int32)
     func adjustSubtitleDelay(by seconds: TimeInterval)
     @discardableResult
